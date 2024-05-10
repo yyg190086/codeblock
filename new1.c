a11220934bd960f50b5dcc4a2fb160f71e1c9640
轮回之境 <1900860488@qq.com>
2024-05-10 21:17:23 +0800

    modify new1

--- new1.c
+++ new1.c
@@ -5,7 +5,7 @@ int main() {
     long f1,f2;
     int i;
     f1 = f2 = 1;
-    for (i = 1; i <= 20; i++) {
+    for (i = 1; i <= 25; i++) {
         printf("%12ld %12ld",f1, f2);
         if(i % 2 == 0) printf("\n");/*控制输出，每行四个*/
         f1 = f1 + f2;/*前两个月加起来赋值给第三个月*/
